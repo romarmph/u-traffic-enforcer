d03b790396ac6cdf4c971f6c76b8a5e75c5db8cb
Romar Macaraeg <roamrcm@gmail.com>
2023-07-28 02:49:56 +0800

    Create enforcer app homepage

---
+++ lib/config/themes/components/fab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../colors.dart';
+import '../spacing.dart';
+import '../textstyles.dart';
+
+const fabTheme = FloatingActionButtonThemeData(
+  backgroundColor: UColors.blue600,
+  foregroundColor: UColors.white,
+);
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,22 +1,330 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:u_traffic_enforcer/config/themes/colors.dart';
+import '../../config/themes/spacing.dart';
+import '../../config/themes/textstyles.dart';
 
-import '../../service/auth_service.dart';
-
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Widget notificationBtn() {
+    return IconButton(
+      onPressed: () {},
+      icon: const Icon(
+        Icons.notifications_outlined,
+        size: USpace.space28,
+      ),
+    );
+  }
+
+  Widget userNav() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: USpace.space16,
+      ),
+      child: Row(
+        children: [
+          ClipOval(
+            child: Image.network(
+              "https://picsum.photos/200",
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(width: USpace.space16),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Enforcer 1",
+                style: const UTextStyle().textlgfontbold.copyWith(
+                      color: UColors.gray700,
+                    ),
+              ),
+              Text(
+                "enforcer1@gmail.com",
+                style: const UTextStyle()
+                    .textxsfontsemibold
+                    .copyWith(color: UColors.gray500),
+              ),
+            ],
+          ),
+          const Spacer(),
+          notificationBtn(),
+        ],
+      ),
+    );
+  }
+
+  Widget viewScheduleBtn() {
+    return TextButton(
+      onPressed: () {},
+      child: const Text("View Schedule"),
+    );
+  }
+
+  Widget miniDashboard() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: USpace.space16,
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(USpace.space12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(USpace.space12),
+          color: UColors.gray100,
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Today's Shift",
+                  style: const UTextStyle().textxsfontnormal.copyWith(
+                        color: UColors.gray600,
+                      ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "On Duty",
+                      style: const UTextStyle().textxsfontnormal.copyWith(
+                            color: UColors.green500,
+                          ),
+                    ),
+                    const SizedBox(width: USpace.space4),
+                    const CircleAvatar(
+                      radius: 5,
+                      backgroundColor: UColors.green500,
+                    )
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(height: USpace.space8),
+            Container(
+              decoration: BoxDecoration(
+                color: UColors.blue600,
+                borderRadius: BorderRadius.circular(USpace.space8),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Jul 28",
+                          style: const UTextStyle().textxlfontmedium.copyWith(
+                                color: UColors.white,
+                              ),
+                        ),
+                        const Spacer(),
+                        Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(USpace.space8),
+                              decoration: BoxDecoration(
+                                color: UColors.white,
+                                borderRadius:
+                                    BorderRadius.circular(USpace.space8),
+                              ),
+                              child: Text(
+                                "5:00 AM",
+                                style: const UTextStyle()
+                                    .textbasefontmedium
+                                    .copyWith(
+                                      color: UColors.gray600,
+                                    ),
+                              ),
+                            ),
+                            const SizedBox(width: USpace.space12),
+                            Text(
+                              "to",
+                              style: const UTextStyle()
+                                  .textbasefontmedium
+                                  .copyWith(
+                                    color: UColors.white,
+                                  ),
+                            ),
+                            const SizedBox(width: USpace.space12),
+                            Container(
+                              padding: const EdgeInsets.all(USpace.space8),
+                              decoration: BoxDecoration(
+                                color: UColors.white,
+                                borderRadius:
+                                    BorderRadius.circular(USpace.space8),
+                              ),
+                              child: Text(
+                                "1:00 PM",
+                                style: const UTextStyle()
+                                    .textbasefontmedium
+                                    .copyWith(
+                                      color: UColors.gray600,
+                                    ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: USpace.space12),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const Icon(
+                          Icons.location_on_outlined,
+                          color: UColors.white,
+                          size: USpace.space28,
+                        ),
+                        const SizedBox(width: USpace.space8),
+                        Text(
+                          "Magic Mall",
+                          style: const UTextStyle().textbasefontmedium.copyWith(
+                                color: UColors.white,
+                              ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: USpace.space12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Next Shift Rotation",
+                      style: const UTextStyle().textxsfontnormal.copyWith(
+                            color: UColors.gray600,
+                          ),
+                    ),
+                    const SizedBox(height: USpace.space4),
+                    Text(
+                      "July 31, 2023",
+                      style: const UTextStyle().textlgfontbold.copyWith(
+                            color: UColors.blue700,
+                          ),
+                    ),
+                  ],
+                ),
+                viewScheduleBtn(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget viewAllBtn() {
+    return TextButton(
+      onPressed: () {},
+      child: const Text("View All"),
+    );
+  }
+
+  Widget recentTicketsList() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(
+              "Ticket #123456",
+              style: const UTextStyle().textbasefontmedium.copyWith(
+                    color: UColors.gray600,
+                  ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget recentTicketView() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: USpace.space16,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Recent Tickets",
+                style: const UTextStyle().textbasefontnormal.copyWith(
+                      color: UColors.gray500,
+                    ),
+              ),
+              const Spacer(),
+              viewAllBtn(),
+            ],
+          ),
+          recentTicketsList(),
+        ],
+      ),
+    );
+  }
+
+  int currentIndex = 0;
+  Widget bottomNav() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+      },
+      selectedItemColor: UColors.blue700,
+      unselectedItemColor: UColors.gray600,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings_outlined),
+          label: "Settings",
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ElevatedButton(
-        onPressed: () {
-          final authService = Provider.of<AuthService>(context, listen: false);
-
-          authService.signOut();
-        },
-        child: const Text("Logout"),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: USpace.space16),
+            userNav(),
+            const SizedBox(height: USpace.space16),
+            miniDashboard(),
+            Expanded(child: recentTicketView()),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: const Text("New Ticket"),
+        icon: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: bottomNav(),
     );
   }
 }
