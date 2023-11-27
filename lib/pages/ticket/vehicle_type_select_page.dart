92bdde58feca7017da3d4ea2f47ba70a7b872846
Romar Macaraeg <roamrcm@gmail.com>
2023-11-27 23:31:38 +0800

    Udpate

---
+++ lib/pages/ticket/vehicle_type_select_page.dart
@@ -0,0 +1,107 @@
+import 'package:u_traffic_enforcer/config/utils/exports.dart';
+
+class VehicleTypeSelectPage extends ConsumerStatefulWidget {
+  const VehicleTypeSelectPage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _VehicleTypeSelectPageState();
+}
+
+class _VehicleTypeSelectPageState extends ConsumerState<VehicleTypeSelectPage>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    print('here');
+    _tabController = TabController(
+      length: 2,
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Select Vehicle Type"),
+      ),
+      body: ref.watch(vehicleTypeStreamProvider).when(
+            data: (data) {
+              final private =
+                  data.where((element) => !element.isPublic).toList();
+              final public = data.where((element) => element.isPublic).toList();
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TabBar(
+                    controller: _tabController,
+                    tabs: const [
+                      Tab(
+                        text: "Public",
+                      ),
+                      Tab(
+                        text: "Private",
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: TabBarView(
+                      controller: _tabController,
+                      children: [
+                        ListView.builder(
+                          itemCount: public.length,
+                          itemBuilder: (context, index) {
+                            final vehicle = public[index];
+                            return ListTile(
+                              visualDensity: VisualDensity.comfortable,
+                              onTap: () {
+                                Navigator.pop(context, vehicle);
+                              },
+                              title: Text(vehicle.typeName),
+                              subtitle: const Text('Tap to select'),
+                              trailing: const Icon(
+                                Icons.touch_app_rounded,
+                                color: UColors.gray300,
+                              ),
+                            );
+                          },
+                        ),
+                        ListView.builder(
+                          itemCount: private.length,
+                          itemBuilder: (context, index) {
+                            final vehicle = private[index];
+                            return ListTile(
+                              visualDensity: VisualDensity.comfortable,
+                              onTap: () {
+                                Navigator.pop(context, vehicle);
+                              },
+                              title: Text(vehicle.typeName),
+                              subtitle: const Text('Tap to select'),
+                              trailing: const Icon(
+                                Icons.touch_app_rounded,
+                                color: UColors.gray300,
+                              ),
+                            );
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+            error: (error, stackTrace) {
+              return const Center(
+                child: Text("Error loading vehicle types"),
+              );
+            },
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+    );
+  }
+}
